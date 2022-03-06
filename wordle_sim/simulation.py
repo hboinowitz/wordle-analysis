d22906f9000146bc5aa034cd9ca2e6d8a9d8d24b
Hannah Boinowitz <hannah.boinowitz@web.de>
2022-03-06 11:08:15 +0100

    Fix handling of yellow letters

--- wordle_sim/simulation.py
+++ wordle_sim/simulation.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple
+from typing import List, Tuple, Set
 import string
 import pandas as pd
 from nltk.corpus import words
@@ -35,23 +35,41 @@ def check_guess(letterbank: List[List[str]], guess: str, correct_word: str) -> L
             colors.append('grey')
     return letterbank, colors
 
-is_suitable = lambda word, letterbank: all([char_ in letterbank[index] for index, char_ in enumerate(word)])  
-def get_suitable_guesses(letterbank: List[List[str]], wordbank: pd.DataFrame):
-    wordbank['is_suitable'] = wordbank['word'].apply(is_suitable, letterbank=letterbank)
+def is_suitable(word: str, letterbank: List[List[str]], yellow_letters: Set[str]) -> bool:
+    for index, char_ in enumerate(word):
+        if not char_ in letterbank[index]:
+            return False
+    for letter in yellow_letters:
+        if not letter in word:
+            return False
+
+    return True
+
+def get_suitable_guesses(letterbank: List[List[str]], wordbank: pd.DataFrame, yellow_letters: List[str]):
+    wordbank['is_suitable'] = wordbank['word'].apply(is_suitable, letterbank=letterbank, 
+                                                     yellow_letters = yellow_letters)
     wordbank = wordbank[wordbank['is_suitable']]
     return list(wordbank['word'])
+
+get_yellow_letters = (
+    lambda letters_with_colors : [letter for letter, color in letters_with_colors.items() if color == 'yellow']
+)
 
 def simulate() -> Tuple[str, List[str], int, bool]:
     letterbank = initialize_letterbank()
     wordbank = initialize_corpus()
     correct_word = wordbank.sample(1).reset_index().loc[0,'word']
     guesses = []
+    yellow_letters = set()
 
     for round in range(6):
-        choices = get_suitable_guesses(letterbank, wordbank)
+        choices = get_suitable_guesses(letterbank, wordbank, yellow_letters)
         guess = np.random.choice(choices)
         guesses.append(guess)
         letterbank, colors = check_guess(letterbank, guess, correct_word)
+        letters_with_colors = dict(zip(list(guess), colors))
+        new_yellow_letters = get_yellow_letters(letters_with_colors)
+        yellow_letters.update(set(new_yellow_letters))
         if guess == correct_word:
             return correct_word, guesses, round + 1, True
 
