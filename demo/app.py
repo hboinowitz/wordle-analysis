fc6484e8845f504c395eec9a4ff86954cfb7efa8
Hannah Boinowitz <hannah.boinowitz@web.de>
2022-03-08 22:34:41 +0100

    Add colors per request

--- demo/app.py
+++ demo/app.py
@@ -1,19 +1,8 @@
-from crypt import methods
-from typing import Dict, List
 from flask import Flask
 from flask import render_template, request
 from wordle_sim import simulate
 
 app = Flask(__name__)
-
-def get_colors_for_guesses(guesses: List[str], keyboards: List[Dict[str, str]]):
-    colors_for_guesses = []
-    for round, guess in enumerate(guesses):
-        colors_for_guess = []
-        for char_ in guess:
-            colors_for_guess.append(keyboards[round + 1][char_])
-        colors_for_guesses.append(colors_for_guess[:])
-    return colors_for_guesses
 
 @app.route("/simulate", methods=['GET', 'POST'])
 def simulation():
@@ -22,8 +11,7 @@ def simulation():
     if request.method == 'POST':
         correct_word = request.form['correct_word'].lower()
         initial_guess = request.form['initial_guess'].lower()
-    correct_word, guesses, rounds, won, letterbanks, keyboards = simulate(correct_word, initial_guess)
-    colors_for_guesses = get_colors_for_guesses(guesses, keyboards)
+    correct_word, guesses, rounds, won, letterbanks, keyboards, colors_for_guesses = simulate(correct_word, initial_guess)
     len_guesses_prior = len(guesses)
     if len_guesses_prior != 6:
         guesses.extend([[" "] * 5] * (6 - len_guesses_prior))
